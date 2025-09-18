7ab029f6c721e2be1ccc2215a1cc91a74338af40
nicolosinatra <nicolo.sinatra2@gmail.com>
2025-09-18 10:17:08 +0200

    fix script

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-document.querySelectorAll('.contact-info p').forEach(function(p) {
-      p.setAttribute('data-marquee', p.textContent);
-    });
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelectorAll('.contact-info p').forEach(function(p) {
+    p.setAttribute('data-marquee', p.textContent);
+  });
+});
 
     
