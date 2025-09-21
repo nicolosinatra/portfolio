1554bb9043bc0c16562e16595c6ca084ec6419cc
nicolosinatra <nicolo.sinatra2@gmail.com>
2025-09-21 13:03:22 +0200

    fix header

--- js/script.js
+++ js/script.js
@@ -10,6 +10,27 @@ $(".backtotop").click(function() {
   $("html").animate({ scrollTop: 0 }, "slow");
 });
 
+$(".plus-icon").on("click", function() {
+  $(this).toggleClass("active");
+});
 
+$(document).ready(function () {
+  $("#menuToggle").on("click", function () {
+    $("#voci-menu").toggleClass("open");
+    if ($("#voci-menu").hasClass("open")) {
+      // mostra con cascata: ritardo progressivo per ogni voce
+      $("#voci-menu .voce-menu").each(function (i) {
+        const delay = i * 0.1; // seconds
+        $(this).css({
+          "transition-delay": delay + "s"
+        });
+      });
+    } else {
+      // reset
+      $("#voci-menu .voce-menu").css({
+        "transition-delay": "0.15s"
+      });
+    }
+  });
+});
 
-    
