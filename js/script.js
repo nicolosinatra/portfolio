3a06adc575cda5c7c79f5fb0764907445bef18e3
nicolosinatra <nicolo.sinatra2@gmail.com>
2025-09-26 15:14:42 +0200

    sticky home

--- js/script.js
+++ js/script.js
@@ -69,3 +69,76 @@ $(document).ready(function () {
     });
   
   });
+
+$(document).ready(function () {
+
+  function getFirstTop() {
+    if ($(window).width() >= 1024) return 55;
+    if ($(window).width() >= 600) return 53;
+    return 47;
+  }
+
+  const $anniProgettiMenu = $(".anni-progetti-menu");
+  const offsets = [];
+
+  // calcolo posizioni e altezza
+  $anniProgettiMenu.each(function(i, h) {
+    offsets.push({
+      el: h,
+      top: $(h).offset().top,
+      index: i,
+    });
+  });
+
+  const headerHeight = $($anniProgettiMenu[0]).outerHeight(true);
+
+  // scroll
+  $(window).on("scroll", function() {
+    const scrollY = $(window).scrollTop();
+    let fixedCount = 0; // fixedCount → tiene conto di quanti titoli sono già stati fissati in alto
+
+    offsets.forEach(function(item) {
+      const $el = $(item.el);
+
+      const topCumulative = fixedCount === 0 ? getFirstTop() : getFirstTop() + fixedCount * headerHeight;
+
+      console.log("topCumulative: " + topCumulative + "fixedCount: " + fixedCount)
+      console.log("scorllY: " + scrollY + " item.top: " + item.top);
+
+
+      if ((scrollY > 100)&&(scrollY >= item.top - topCumulative)) {
+        $el.addClass("fixed").css({ top: topCumulative + "px" });
+        fixedCount++;
+      } else {
+        $el.removeClass("fixed").css({ top: "" });
+      }
+    });
+  });
+
+  // resize
+  $(window).on("resize", function() {
+    // qui puoi ricalcolare gli offsets se necessario
+    offsets.forEach(function(item) {
+      item.top = $(item.el).offset().top;
+    });
+    $(window).trigger("scroll"); // aggiorna subito le posizioni
+  });
+
+  // smooth scroll
+  $anniProgettiMenu.on("click", function() {
+    const targetId = $(this).data("target");
+    if (targetId) {
+      // Altezza cumulativa dei titoli sticky attuali
+      let totalStickyHeight = 0;
+
+      $(".anni-progetti-menu.fixed").each(function() {
+        totalStickyHeight += $(this).outerHeight(true);
+      });
+
+      const targetOffset = $("#" + targetId).offset().top - totalStickyHeight- 15; // -5px extra per margine
+      $("html, body").animate({ scrollTop: targetOffset }, 500);
+    }
+    $(this).removeClass("fixed").css({ top: "" });
+  });
+
+});
