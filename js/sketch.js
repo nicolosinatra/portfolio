4b9cc029901b5b430e34d4e39297e841ca960f3c
nicolosinatra <nicolo.sinatra2@gmail.com>
2025-10-15 13:47:07 +0200

    fix

--- js/sketch.js
+++ js/sketch.js
@@ -5,6 +5,13 @@ let angle = 360 / symmetry;
 // variabili per smoothing
 let smoothX, smoothY;
 
+// variabili per blink effect
+// let opacity = 0;
+let mouseXPrev = 0;
+let mouseYPrev = 0;
+let showBlink = true;  // mostra il blink finché il mouse non si muove
+let mouseMovedOnce = false;
+
 function setup() {
   if (windowWidth < 600) {
     createCanvas(windowWidth, windowHeight - 47 - 28);
@@ -25,37 +32,55 @@ function setup() {
 function draw() {
   translate(width / 2, height / 2);
 
-  if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
-    // ammorbidisco le coordinate con lerp
-    smoothX = lerp(smoothX, mouseX, 0.4);
-    smoothY = lerp(smoothY, mouseY, 0.4);
+  if (showBlink && !mouseMovedOnce) {
+    background("#00f9ff");
 
-    // coordinate relative al centro
-    let dotX = round(smoothX - width / 2);
-    let dotY = round(smoothY - height / 2);
+    // effetto pulsazione: opacity va avanti e indietro
+    // opacity = 128 + 127 * sin(frameCount * 2);
 
-    // reset sfondo con click
-    if (windowWidth > 600 && mouseIsPressed) {
-      background("#00f9ff");
+    noStroke();
+    fill(2, 21, 11);
+    ellipse(0, 0, 10 + 10 * sin(frameCount * 2));
+
+    // controlla se il mouse si muove
+    if (mouseX !== mouseXPrev || mouseY !== mouseYPrev) {
+      mouseMovedOnce = true;
+      fadingOut = true;
     }
+    return; // non disegnare altro
+  }else {
+    if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
+      // ammorbidisco le coordinate con lerp
+      smoothX = lerp(smoothX, mouseX, 0.4);
+      smoothY = lerp(smoothY, mouseY, 0.4);
 
-    // disegno i punti in tutti i settori
-    for (let i = 0; i < symmetry; i++) {
-      push();
+      // coordinate relative al centro
+      let dotX = round(smoothX - width / 2);
+      let dotY = round(smoothY - height / 2);
 
-      rotate(i * angle);
+      // reset sfondo con click
+      if (windowWidth > 600 && mouseIsPressed) {
+        background("#00f9ff");
+      }
 
-      noStroke();
-      fill("#26150B");
-      ellipse(dotX, dotY, 3, 3); // pallino principale
+      // disegno i punti in tutti i settori
+      for (let i = 0; i < symmetry; i++) {
+        push();
 
-      // riflesso
-      push();
-      scale(1, -1);
-      ellipse(dotX, dotY, 3, 3);
-      pop();
+        rotate(i * angle);
 
-      pop();
+        noStroke();
+        fill("#26150B");
+        ellipse(dotX, dotY, 3, 3); // pallino principale
+
+        // riflesso
+        push();
+        scale(1, -1);
+        ellipse(dotX, dotY, 3, 3);
+        pop();
+
+        pop();
+      }
     }
   }
 }
@@ -63,3 +88,4 @@ function draw() {
 function windowResized() {
   setup();
 }
+
